dd900d9ef89777174f867163fe7b71ab5adf699f
Ivana Jankovic <ivanaj0610@gmail.com>
2022-11-14 16:08:34 +0100

    Fix login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import { environment } from "src/environments/environment";
-import { UserInfo } from "src/app/model/user.model";
-import {LoginService} from "../../service/login.service";
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { UserInfo } from 'src/app/model/user.model';
+import { LoginService } from '../../service/login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
   userInfo = new UserInfo();
@@ -19,12 +19,11 @@ export class LoginComponent implements OnInit {
     //return this._http.get<any>(environment.backendUrl + 'api/user/getUser').subscribe();
   }
 
-  login(){
-    console.log(this.userInfo.email)
-      this.loginService.login(this.userInfo).subscribe(
-        (data) => (console.log("da"+data)),
-        (err) => (console.log("ne"+err)),
-      );
+  login() {
+    console.log(this.userInfo.username);
+    this.loginService.login(this.userInfo).subscribe(
+      (data) => console.log('da' + data),
+      (err) => console.log('ne' + err)
+    );
   }
-
 }
--- src/app/model/user.model.ts
+++ src/app/model/user.model.ts
@@ -1,4 +1,3 @@
-export class UserInfo{
-  constructor(public email: string = '', public password: string = '') {
-  }
+export class UserInfo {
+  constructor(public username: string = '', public password: string = '') {}
 }
